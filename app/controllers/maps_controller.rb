87a8d622cebf6da4ef3855acbb1f717df83adfd0
Max Ken <tango12speedcell@gmail.com>
2020-06-01 15:57:19 +0900

    add link_to infowindow

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -7,7 +7,7 @@ class MapsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@map_index) do |map, marker|
       marker.lat map.latitude
       marker.lng map.longitude
-      marker.infowindow map.title.title
+      marker.infowindow render_to_string(partial: 'maps/infowindow', locals: {map: map})
     end
   end
 
