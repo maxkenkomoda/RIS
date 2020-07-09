3f3da73df5bcf89b1655050f58f84d5ee1b16e24
Max Ken Komoda <tango12speedcell@gmail.com>
2020-07-10 04:28:51 +0900

    delete unnecessary migration file

--- db/migrate/20200521055539_drop_traffic.rb
+++
@@ -1,5 +0,0 @@
-class DropTraffic < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :traffics
-  end
-end
--- db/migrate/20200602090225_dropshop.rb
+++
@@ -1,5 +0,0 @@
-class Dropshop < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :shops
-  end
-end
